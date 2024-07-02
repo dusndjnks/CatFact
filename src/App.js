7eaac1bfaae93cf1e0c88511b35284e0bf0931e8
dusndjnks <mhdaffan0000@gmail.com>
2024-07-02 15:25:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [catFact,setCatFact]=useState("")
+  const fetchData=async()=>{
+    const {data}=await axios.get("https://catfact.ninja/fact")
+    setCatFact(data.fact)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen gap-10 flex flex-col justify-center items-center bg-slate-300">
+      {/* <div className="bg-black text-white  px-5 py-2 text-3xl">
+        <span className="loading-text-words">C</span>
+        <span className="loading-text-words">L</span>
+        <span className="loading-text-words">I</span>
+        <span className="loading-text-words">C</span>
+        <span className="loading-text-words">K</span>
+      </div> */}
+      <div className="">
+      <button className="bg-black text-white px-3 py-1 text-3xl font-semibold  rounded-lg" onClick={fetchData}>Click</button>
+      </div>
+      <div>
+      <h1 className="text-black px-3 py-2 bg-white w-[500px] text-center">{catFact}</h1>
+      </div>
     </div>
   );
 }
